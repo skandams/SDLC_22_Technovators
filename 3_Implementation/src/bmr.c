76ef1cb3d30cb77a64a77f461061f3633f54e99a
Sivapriyapj <pjsivapriya@gmail.com>
2021-08-22 00:26:17 +0530

    code

--- 3_Implementation/src/bmr.c
+++ 3_Implementation/src/bmr.c
@@ -1,13 +1,13 @@
 
 #include "header.h"
-float bmr(float weight, float height,int age,char *gender){
+float bmr(float weight, float height,int age,char gender){
 
 
     float bmr;
-    if(*gender ==("F" || "f")){
+    if(gender == 'f'){
         bmr=10*weight+6.25*height-5*age+5;
     }
-    else if(*gender==("M" || "m"))
+    else if(gender== 'm')
     {
         bmr=10*weight+6.25*height-5*age-161;
     }
